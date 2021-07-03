345a9c47d331881779cbb6f10879bdb5d9b550b2
kimjunseo <air.junseo@gmail.com>
2021-07-03 17:25:09 +0900

    refactor: deleteQuestion 메서드 로직 도메인에서 처리
    QnaService -> deleteQuestion()

--- src/main/java/qna/question/QnaService.java
+++ src/main/java/qna/question/QnaService.java
@@ -4,16 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import qna.answer.Answer;
-import qna.common.ContentType;
 import qna.common.exception.CannotDeleteException;
 import qna.common.exception.NotFoundException;
 import qna.deletehistory.DeleteHistory;
 import qna.deletehistory.DeleteHistoryService;
 import qna.user.User;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,25 +39,8 @@ public class QnaService {
     @Transactional
     public void deleteQuestion(User loginUser, Long questionId) throws CannotDeleteException {
         Question question = findQuestionById(questionId);
-        if (!question.isOwner(loginUser)) {
-            throw new CannotDeleteException("질문을 삭제할 권한이 없습니다.");
-        }
-
-        List<Answer> answers = question.getNotDeletedAnswers();
-
-        for (Answer answer : answers) {
-            if (!answer.isOwner(loginUser)) {
-                throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
-            }
-        }
-
-        List<DeleteHistory> deleteHistories = new ArrayList<>();
-        question.setDeleted(true);
-        deleteHistories.add(new DeleteHistory(ContentType.QUESTION, questionId, question.getWriter(), LocalDateTime.now()));
-        for (Answer answer : answers) {
-            answer.setDeleted(true);
-            deleteHistories.add(new DeleteHistory(ContentType.ANSWER, answer.getId(), answer.getWriter(), LocalDateTime.now()));
-        }
+        question.delete(loginUser);
+        List<DeleteHistory> deleteHistories = DeleteHistory.createDeleteHistories(question);
         deleteHistoryService.saveAll(deleteHistories);
     }
 }
